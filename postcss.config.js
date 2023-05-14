71ad673f931dab52ac63ed31d54207a095ed0853
Yudi <renanyaguinuma@hotmail.com>
2023-05-14 06:59:56 -0300

    Remove cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
 import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import cssnanoPlugin from 'cssnano';
 
 /** @type {import('postcss-load-config').Config} */
 export default {
-	plugins: [
-		tailwind,
-		autoprefixer,
-		...(process.env.NODE_ENV === 'production' ? [cssnanoPlugin] : [])
-	]
+	plugins: [tailwind, autoprefixer]
 };
