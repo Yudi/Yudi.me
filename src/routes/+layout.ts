1387e92ea0ff40e75dd0978fc1745df255626fc8
Yudi <renanyaguinuma@hotmail.com>
2023-06-01 00:45:29 -0300

    Update layout to add i18n

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,13 @@
 import '$lib/styles/global.postcss';
+
+import { loadTranslations, locale } from '$lib/i18n/i18n';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async ({ url }) => {
+  const { pathname } = url;
+  const defaultLocale = 'en';
+  const initLocale = locale.get() || defaultLocale;
+  await loadTranslations(initLocale, pathname);
+
+  return {};
+}
