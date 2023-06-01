8b93442a9ca17751b9e058a875e5330fc9cf19fd
Yudi <renanyaguinuma@hotmail.com>
2023-06-01 00:47:18 -0300

    Add i18n (#1)

--- src/components/shared/navbar.svelte
+++ src/components/shared/navbar.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
-	import { ChevronDown, ChevronRight, Menu } from 'lucide-svelte';
+	import { Menu, Languages, ChevronDown } from 'lucide-svelte';
+	import { t, locale } from '$lib/i18n/i18n';
 
 	const items: Items[] = [
 		{
-			name: 'Digital garden',
-			children: {
-				Pessoal: {
+			name: 'common.navbar.digitalGarden',
+			children: [
+				{
+					name: 'common.navbar.digitalGarden.personal',
 					url: 'https://personal.yudi.me'
 				},
-				Estudos: {
+				{
+					name: 'common.navbar.digitalGarden.studying',
 					url: 'https://studying.yudi.me'
 				}
-			}
+			]
 		}
 	];
 
 	interface Items {
 		name: string;
-		children?: {
-			[key: string]: {
-				url?: string;
-			};
-		};
 		url?: string;
+		children?: {
+			name: string;
+			url?: string;
+		}[];
 	}
 </script>
 
@@ -30,25 +32,23 @@
 <div class="navbar border-b-2 bg-base-100">
 	<div class="navbar-start">
 		<!-- Mobile -->
-
 		<div class="dropdown">
 			<label tabindex="0" class="btn-ghost btn lg:hidden">
 				<Menu class="h-5 w-5" />
 			</label>
 			<ul
 				tabindex="0"
-				class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+				class="dropdown-content menu rounded-box menu-sm mt-3 w-52 bg-base-100 p-2 shadow"
 			>
 				{#each items as item}
 					{#if item.children}
 						<li tabindex="0">
 							<a class="justify-between">
 								{item.name}
-								<ChevronRight class="h-5 w-5" />
 							</a>
-							<ul class="bg-base-100 p-2">
-								{#each Object.entries(item.children) as [key, child]}
-									<li><a href={child.url || undefined}>{key}</a></li>
+							<ul class="p-2">
+								{#each item.children as child}
+									<li><a href={child.url || undefined}>{child.name}</a></li>
 								{/each}
 							</ul>
 						</li>
@@ -58,7 +58,6 @@
 				{/each}
 			</ul>
 		</div>
-
 		<a class="btn-ghost btn text-xl normal-case" href="/">Yudi</a>
 	</div>
 
@@ -67,21 +66,28 @@
 		<ul class="menu menu-horizontal px-1">
 			{#each items as item}
 				{#if item.children}
-					<li tabindex="0">
-						<a>
-							{item.name}
+					<li tabindex="0" class="dropdown">
+						<label tabindex="0">
+							{$t(item.name)}
 							<ChevronDown class="h-5 w-5" />
-						</a>
-						<ul class="bg-base-100 p-2">
-							{#each Object.entries(item.children) as [key, child]}
-								<li><a href={child.url || undefined}>{key}</a></li>
+						</label>
+						<ul class="dropdown-content menu rounded-box bg-base-100 p-2 shadow">
+							{#each item.children as child}
+								<li><a href={child.url || undefined}>{$t(child.name)}</a></li>
 							{/each}
 						</ul>
 					</li>
 				{:else}
-					<li><a href={item.url || undefined}>{item.name}</a></li>
+					<li><a href={item.url || undefined}>{$t(item.name)}</a></li>
 				{/if}
 			{/each}
+			<li tabindex="0" class="dropdown-end dropdown">
+				<label tabindex="0"> <Languages class="h-5 w-5" /></label>
+				<ul class="dropdown-content menu rounded-box bg-base-100 p-2 shadow">
+					<li><a on:click={() => locale.set('en')}>English</a></li>
+					<li><a on:click={() => locale.set('pt')}>Português</a></li>
+				</ul>
+			</li>
 		</ul>
 	</div>
 </div>
---
+++ src/lib/i18n/i18n.ts
@@ -0,0 +1,32 @@
+import i18n from 'sveltekit-i18n';
+import type { Config } from 'sveltekit-i18n';
+
+const config: Config = {
+	loaders: [
+		{
+			locale: 'en',
+			key: 'common',
+			loader: async () => (await import('./en/common.json')).default
+		},
+		{
+			locale: 'en',
+			key: 'home',
+			routes: ['/'],
+			loader: async () => (await import('./en/home.json')).default
+		},
+
+		{
+			locale: 'pt',
+			key: 'common',
+			loader: async () => (await import('./pt/common.json')).default
+		},
+		{
+			locale: 'pt',
+			key: 'home',
+			routes: ['/'],
+			loader: async () => (await import('./pt/home.json')).default
+		}
+	]
+};
+
+export const { t, locale, locales, loading, loadTranslations } = new i18n(config);
