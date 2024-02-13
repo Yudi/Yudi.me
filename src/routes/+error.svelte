2fd147be7ff618412f166e8f93243fae6d044241
Yudi <12040397+Yudi@users.noreply.github.com>
2024-02-13 20:05:09 -0300

    Remove 404 description

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,7 +7,6 @@
 	<div class="justify-top flex flex-1 flex-col items-center pt-12 text-xl">
 		{#if $page.status === 404}
 			<h1 class="font-semibold">{$t('common.error.notFound')}</h1>
-			<h2 class="font-normal">{$t('common.error.notFound.description')}</h2>
 		{:else}
 			<h1 class="font-semibold">{$page.status || $t('common.error.else.unknown')}</h1>
 			<h2 class="font-normal">
