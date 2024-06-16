562ae4f3ba9019b46e82e7dbb339737a79b20ec0
Yudi <12040397+Yudi@users.noreply.github.com>
2024-06-16 19:02:31 -0300

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,27 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), svelte()],
+  site: 'https://yudi.com.br',
+  integrations: [mdx(), tailwind(), svelte(), sitemap({
+    i18n: {
+      defaultLocale: "en",
+      locales: ["en", "pt-br"],
+      fallback: {
+        "pt-br": "en"
+      }
+    }
+  })],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt-br"],
-  },
+    fallback: {
+      "pt-br": "en"
+    }
+  }
 });
