4b1cdce0d1004dfcd574b75d0aa45307d40409a7
Yudi <12040397+Yudi@users.noreply.github.com>
2025-01-17 16:01:59 -0300

    Fix "en" invalid locale

--- src/components/resources/shared/date.service.ts
+++ src/components/resources/shared/date.service.ts
@@ -2,10 +2,10 @@ import { format, type Locale } from "date-fns";
 import { enUS, ptBR } from "date-fns/locale";
 
 export function formatDate(date: string, currentLocale: string, formatting: string) {
-  const locales: { [key: string]: Locale } = { "pt-br": ptBR, "en-us": enUS };
+  const locales: { [key: string]: Locale } = { "pt-br": ptBR, "en-us": enUS, en: enUS };
   const lowercaseCurrentLocale = currentLocale.toLowerCase();
   if (!lowercaseCurrentLocale || !locales[lowercaseCurrentLocale]) {
-    throw new Error("Invalid locale");
+    throw new Error("Invalid locale" + lowercaseCurrentLocale);
   }
 
   const locale = locales[currentLocale.toLowerCase()] || enUS;
