5379588ea6f21332bd52bef83be754971d67e76a
Yudi <12040397+Yudi@users.noreply.github.com>
2025-01-29 13:20:22 -0300

    Enable Astro SVG experimental feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,23 +8,31 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yudi.com.br',
-  integrations: [mdx(), tailwind(), svelte(), sitemap({
-    i18n: {
-      defaultLocale: "en",
-      locales: {
-        "en": "en", 
-        "pt-br": "pt-br"
-      }
-    },
-  })],
+  site: "https://yudi.com.br",
+  integrations: [
+    mdx(),
+    tailwind(),
+    svelte(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          "pt-br": "pt-br",
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pt-br"],
     fallback: {
-      "pt-br": "en"
-    }
+      "pt-br": "en",
+    },
   },
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  experimental: {
+    svg: true,
+  },
 });
